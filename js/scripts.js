858f32dd04097ccfb8c5f147837c2ced17e627d4
Dan Lauby and Matt Kelley <matt@>
2017-01-26 11:17:10 -0800

    add a win or tie checker function

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,11 @@ function Player(playerName, symbol, turn) {
 }
 
 function Board() {
-  this.board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+  this.board = {
+    column00: 0, column01: 1, column02: 2,
+    column10: 3, column11: 4, column12: 5,
+    column20: 6, column21: 7, column22: 8
+  };
 }
 
 function Game(board, player1, player2) {
@@ -15,7 +19,40 @@ function Game(board, player1, player2) {
 }
 
 Game.prototype.clearBoard = function() {
-  activeBoard.board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+  this.board = {
+    column00: 0, column01: 1, column02: 2,
+    column10: 3, column11: 4, column12: 5,
+    column20: 6, column21: 7, column22: 8
+  };
+}
+
+Game.prototype.checkWin = function() {
+  // console.log(this.board);
+  if (this.board.column00 === this.board.column01 && this.board.column00 === this.board.column02) {
+    console.log('someone wins first row');
+  }else if (this.board.column10 === this.board.column11 && this.board.column10 === this.board.column12) {
+    console.log('someone wins second row');
+  }else if (this.board.column20 === this.board.column21 && this.board.column20 === this.board.column22) {
+    console.log('someone wins third row');
+  }else if (this.board.column00 === this.board.column10 && this.board.column00 === this.board.column20) {
+    console.log('someone wins first column');
+  }else if (this.board.column01 === this.board.column11 && this.board.column01 === this.board.column21) {
+    console.log('someone wins second column');
+  }else if (this.board.column02 === this.board.column12 && this.board.column02 === this.board.column22) {
+    console.log('someone wins third column');
+  }else if (this.board.column00 === this.board.column11 && this.board.column00 === this.board.column22) {
+    console.log('someone wins backslash');
+  }else if (this.board.column02 === this.board.column11 && this.board.column02 === this.board.column20) {
+    console.log('someone wins forwardslash');
+  }
+
+  //checks for tie game
+  var objValues = Object.values(this.board).join("");
+  var reg = new RegExp(/[0-8]/g);
+  var search = reg.exec(objValues);
+  if (search === null) {
+    console.log("Tie game");
+  }
 }
 
 
@@ -35,17 +72,17 @@ Game.prototype.setGame = function() {
   for (i=0 ; i<=2 ; i++) {
     $('.container').append('<div class="row" id="row' + i + '"></div>');
       for (j=0; j<=2; j++) {
-        $('#row' + i).append('<div class="col-xs-4" id="column' + i + "-" + j + '"></div>');
+        $('#row' + i).append('<div class="col-xs-4" id="column' + i + j + '"></div>');
       }
   }
-  // newGame.clearBoard();
+  this.clearBoard();
   return "setgame works";
 }
 
-Game.prototype.fillBoard = function(){
-  $("#column0-0").append("<p>X</p>");
-  return "fill works";
-}
+// Game.prototype.fillBoard = function(){
+//   $("#column0-0").append("<p>X</p>");
+//   return "fill works";
+// }
 
 $(function(){
   var player1 = new Player('Dan', 'X',true);
@@ -57,13 +94,21 @@ $(function(){
   $(".col-xs-4").click(function(){
     console.log(this.id);
     if(player1.turn === true){
-      $("#" + this.id).append("<p>X</p>");
-      player1.turn = false;
-      player2.turn = true;
+      if($(this).children().length===0){
+        $("#" + this.id).append("<p>X</p>");
+        newGame.board[this.id] = 'X';
+        player1.turn = false;
+        player2.turn = true;
+        newGame.checkWin();
+      }
     }else{
-      $("#" + this.id).append("<p>0</p>");
-      player1.turn = true;
-      player2.turn = false;
+      if($(this).children().length===0){
+        $("#" + this.id).append("<p>0</p>");
+        newGame.board[this.id] = 'O';
+        player1.turn = true;
+        player2.turn = false;
+        newGame.checkWin();
+      }
     };
   })
 
